d4dbc9147ffce9dd7974122dfe5b5c52b12ba629
vpbmac <vitorpbarbosa7@gmail.com>
2024-10-05 11:13:26 -0300

    ps4c start

---
+++ psets/ps4/ps4/ps4a_solve/ps4a.py
@@ -0,0 +1,116 @@
+# Problem Set 4A
+# Name: <your name here>
+# Collaborators:
+# Time Spent: x:xx
+import unittest
+
+
+def executor(left, right):
+
+    permutations = []
+
+    n = len(right) + 1
+    for i in range(len(right)+1):
+
+        if i == 0:
+            word = left + right
+
+        elif i > 0 and i < n:
+            word = right[:i] + left + right[i:]
+
+        else:
+            word = right + left
+        
+        permutations.append(word)
+
+    # print(permutations)
+
+    return permutations
+
+def _aux(left, right):
+
+    print(f''' Left {left} and right {right}''')
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+
+    if len(sequence) == 1:
+        return sequence
+
+    else:
+        left = sequence[0]
+        right = sequence[1:]
+
+        # recursive call for right permutations
+        # em todos os niveis, tenho left e right
+
+        # esquerda motra que ta voltando 
+        # direta mostra que ta indo 
+        right_permutations = get_permutations(right)
+
+        # quando volto, o que tenho que fazer?
+        permutations = []
+
+        # fazer algo com o que está voltando, que é o right_permutation
+        # pegando todas permutacoes que vieram da direita, quando volta
+        for single_right_permutation in right_permutations:
+
+            new_permutations = executor(left, single_right_permutation)
+
+            for new_permutation in new_permutations:
+
+                permutations.append(new_permutation)
+
+        # onde eu sempre tenho dificuldade, de voltar
+        return permutations
+
+class TestGetPermutations(unittest.TestCase):
+
+    def test_case_1(self):
+        example_input = 'abc'
+        expected_output = set(['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+        self.assertEqual(set(get_permutations(example_input)), expected_output)
+
+    def test_case_2(self):
+        # Example Test Case 2
+        example_input = 'ab'
+        expected_output = set(['ab', 'ba'])
+        self.assertEqual(set(get_permutations(example_input)), expected_output)
+
+    def test_case_3(self):
+        # Example Test Case 3
+        example_input = 'a'
+        expected_output = set(['a'])
+        self.assertEqual(set(get_permutations(example_input)), expected_output)
+
+
+if __name__ == '__main__':
+#    #EXAMPLE
+#    example_input = 'abc'
+#    print('Input:', example_input)
+#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+#    print('Actual Output:', get_permutations(example_input))
+    
+#    # Put three example test cases here (for your sanity, limit your inputs
+#    to be three characters or fewer as you will have n! permutations for a 
+#    sequence of length n)
+
+    unittest.main()
+
--- psets/ps4/ps4/ps4c.py
+++ psets/ps4/ps4/ps4c.py
@@ -70,15 +70,16 @@ class SubMessage(object):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
-    
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME) 
+
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
         
         Returns: self.message_text
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text
 
     def get_valid_words(self):
         '''
